532fb24745daef121f7b5225f5df22d217a9db75
Mobbzz <shad_1994@hotmail.com>
2024-02-04 21:20:11 +0100

    finish

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import {Route, BrowserRouter as Router, Routes} from 'react-router-dom'
+import { Navbar } from './components/Navbar'
+import About from './Pages/About'
+import Shop from './pages/Shop'
+import NotFound from './pages/NotFound'
+import { ShoppingCartPage } from './Pages/ShoppingCartPage'
+import  Footer  from './Components/Footer'
+
+import ProductDetails from './pages/ProductDetails' 
+import Home from './Pages/Home'
+
+function App() {
+  return (
+    <Router> 
+      <Navbar />
+
+
+      <div className="container">
+
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/details/:id' element={<ProductDetails /> } />  
+          <Route path='/About' element={<About /> } />
+          <Route path='/ShoppingCart' element={<ShoppingCartPage />} />
+          
+          <Route path='*' element={ <NotFound />} />
+        </Routes> 
+        
+
+      </div>
+        
+      
+    <Footer /> 
+    </Router>
+    
+  )
+}
+export default App
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const products = [
+
+  
+];
+
+function Home() {
+  return (
+    <div className="home">
+    <h1 className="home-title">Välkommen till Siba!</h1>
+    <div className="home-background">
+      <img src="https://i0.wp.com/www.anactazia.se/wp-content/uploads/2018/12/indesign-siba2-1451x1000px.png?fit=1451%2C1000" alt="Siba" />
+    </div>
+  </div>
+          
+  );
+}
+
+export default Home;
